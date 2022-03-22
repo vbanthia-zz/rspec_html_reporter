4102e61d6c826e0d2eb90441655f0db3734126e2
Carlos Gutierrez <carlos@spartan-testsolutions.co.uk>
2022-03-22 20:20:21 +0000

    Adjusted spacing

--- lib/rspec_pretty_html_reporter/helpers.rb
+++ lib/rspec_pretty_html_reporter/helpers.rb
@@ -9,6 +9,7 @@ module Helpers
   # Renames duplicate filenames for specs that have the same description
   # @param [String] filename  Duplicate filename to rename
   # @return [String]          Renamed filename
+  #
   def self.rename_duplicate_filename(filename)
     if File.exist?(filename)
       base, ext = /\A(.+?)(\.[^.]+)?\Z/.match(filename).to_a[1..]
@@ -26,6 +27,7 @@ module Helpers
   # @param [Hash] group_name    Group name to check for duplicate keys
   # @param [String] description Duplicate description to rename
   # @return [String]            Renamed description
+  #
   def self.rename_duplicate_description(group_name, description)
     if group_name.key?(description)
       number = group_name.select { |k, _v| k.include?(description) }.length
